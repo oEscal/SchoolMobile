c02e5531556a4e0ccfdff7ee268b4b945d924b2b
Rafael Simões <rafaeljsimoes@gmail.com>
2019-05-14 03:35:37 +0100

    Ementa Page-> Date added

--- app/src/main/java/com/example/SchoolMobile/EmentaPage.java
+++ app/src/main/java/com/example/SchoolMobile/EmentaPage.java
@@ -21,6 +21,7 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -33,12 +34,7 @@ public class EmentaPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        Calendar c = Calendar.getInstance();
 
-        Date date = new Date();
-
-        c.setTime(date);
-        int dayofWeek = c.get(Calendar.DAY_OF_WEEK);
 
         menus = Fill_Info.fill_Menu();
 
@@ -48,7 +44,7 @@ public class EmentaPage extends AppCompatActivity {
 
         TableLayout mainTable = (TableLayout) findViewById(R.id.mainTable);
 
-        set_Menus(mainTable, this_object,dayofWeek-2);
+        set_Menus(mainTable, this_object);
     }
 
     public TextView[] getTextViews(TableLayout mainTable, int i) {
@@ -72,20 +68,50 @@ public class EmentaPage extends AppCompatActivity {
     }
 
 
-    public void set_Menus(TableLayout mainTable, EmentaPage this_object,int week_day) {
+    public void set_Menus(TableLayout mainTable, EmentaPage this_object) {
 
+        Date date = new Date();
+        Calendar c = Calendar.getInstance();
+
+        c.setTime(date);
+
+        int week_day = c.get(Calendar.DAY_OF_WEEK) -2 ;
+
+        List<Integer> day_incrementer = new ArrayList<>();
+        int counter_off=0;
+        int counter=-1;
         for (int i = 0; i < 5; i++) {
             View tr = LayoutInflater.from(this_object).inflate(R.layout.menurow, null, false);
-            if (week_day > i )
+            if (week_day > i ){
+                counter_off+=1;
                 tr.setAlpha((float) 0.2);
+                day_incrementer.add(counter_off * (-1));
+            }else {
+                counter+=1;
+                day_incrementer.add(counter);
+            }
+
             mainTable.addView(tr);
         }
+
+        Collections.sort(day_incrementer);
+
+        System.out.println(day_incrementer);
         List<String> keys = new ArrayList<String>(menus.keySet());
 
         for (int i = 0; i < keys.size(); i++) {
+
+            c.setTime(date);
+            c.add(Calendar.DATE,day_incrementer.get(i));
+            c.setTime(c.getTime());
+
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH) +1 ;
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
             TextView all_views[] =this.getTextViews(mainTable,i);
             String key= keys.get(i);
-            all_views[0].setText(key);
+            all_views[0].setText(key+" "+day+"-"+month+"-"+year);
             all_views[1].setText("Sopa: "+menus.get(key).getSopa());
             all_views[1].setTextSize(20);
             all_views[2].setText("Prato: "+menus.get(key).getPrato());
